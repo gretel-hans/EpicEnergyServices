f9909c3ae8ddd7b611f7d9e0567e316852806653
promi93 <116350522+promi93@users.noreply.github.com>
2023-06-20 16:26:25 +0200

    problema import csv su tab comuni

--- src/main/java/com/hans/model/Comune.java
+++ src/main/java/com/hans/model/Comune.java
@@ -1,11 +1,12 @@
 package com.hans.model;
 
-import java.util.List;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,7 +26,10 @@ public class Comune {
 	
 	private String nome;
 	
-	@OneToMany
-	private List<Provincia> provinca;
+	@ManyToOne
+	private Provincia provinca;
+	
+	@Column(name = "denominazione_provincia")
+	private String denominazioneProvincia;
 	
 }
