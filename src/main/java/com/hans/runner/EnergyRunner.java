ac00f4bdd8eae7be6d90d558c7f822a05914068d
gretel-hans <adjeihansel@icloud.com>
2023-06-21 14:44:39 +0200

    funzione lettura csv e scrittura su tabella

--- src/main/java/com/hans/runner/EnergyRunner.java
+++ src/main/java/com/hans/runner/EnergyRunner.java
@@ -1,6 +1,7 @@
 package com.hans.runner;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,71 +16,59 @@ import com.hans.repository.ProvinciaRepository;
 import com.hans.repository.StatoFatturaRepository;
 
 @Component
-public class EnergyRunner implements CommandLineRunner{
-	
+public class EnergyRunner implements CommandLineRunner {
+
 	@Autowired
 	StatoFatturaRepository statoFatturaRepository;
 
 	@Autowired
 	ProvinciaRepository provinciaRepository;
 
-
-	
-	
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Runner epic energy sevrices.... ");
-		
-		if(statoFatturaRepository.findAll().size() == 0) {
+
+		if (statoFatturaRepository.findAll().size() == 0) {
 			setStatoFatture();
 		}
 
-		if(provinciaRepository.findAll().size() == 0){
-			String provinceTotali;
-			String[] rigaProvincia;
-			String[] unicaProvincia;
-			
-			//parsing a CSV file into Scanner class constructor  
-			Scanner sc = new Scanner(new File("comuniEProvince/province-italiane.csv"));  
-			sc.useDelimiter(",");   //sets the delimiter pattern  
-			while (sc.hasNext())  //returns a boolean value  
-			{  
-				provinceTotali=sc.next();
-				rigaProvincia=provinceTotali.split("\n");
-				for(int i=0;i<rigaProvincia.length;i++){
-					//System.out.println(rigaProvincia.length);
-					//System.out.println((i+1)+": "+rigaProvincia[i]);
-					unicaProvincia=rigaProvincia[i].split(";");
-					
-					Provincia p= new Provincia(null,unicaProvincia[0],unicaProvincia[1],unicaProvincia[2]);
-									//System.out.println("Sigla: "+unicaProvincia[0]+" nome provincia:"+unicaProvincia[1]+" nome regione:"+unicaProvincia[2]); 
-									//p.setSigla(unicaProvincia[0]);
-									//p.setNome(unicaProvincia[1]);
-									//p.setRegione(unicaProvincia[2]);
-									provinciaRepository.save(p);
-									//System.out.println(p);
-			
-								
-								//System.out.println();
-			
-						}
-						
-					}
-						sc.close();  //closes the scanner  
+		if (provinciaRepository.findAll().size() == 0) {
+			setProvince();
 		}
-			}   
-			
 
+	}
 
-		
-	
-	
-	
 	public void setStatoFatture() {
 		StatoFattura st = new StatoFattura(null, Stato_Fatture.PAGATO);
 		StatoFattura st2 = new StatoFattura(null, Stato_Fatture.NON_PAGATO);
 		statoFatturaRepository.save(st);
 		statoFatturaRepository.save(st2);
+	}
+
+	public void setProvince() {
+		String provinceTotali;
+		String[] rigaProvincia;
+		String[] unicaProvincia;
+		// parsing a CSV file into Scanner class constructor
+		Scanner sc;
+		try {
+			sc = new Scanner(new File("comuniEProvince/province-italiane.csv"));
+			sc.useDelimiter(","); // sets the delimiter pattern
+			while (sc.hasNext()) // returns a boolean value
+			{
+				provinceTotali = sc.next();
+				rigaProvincia = provinceTotali.split("\n");
+				for (int i = 0; i < rigaProvincia.length; i++) {
+					unicaProvincia = rigaProvincia[i].split(";");
+					Provincia p = new Provincia(null, unicaProvincia[0], unicaProvincia[1], unicaProvincia[2]);
+					provinciaRepository.save(p);
+				}
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
